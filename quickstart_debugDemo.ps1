478a8322635bf227a6e655f4e91857ef7cf1c1f4
Catherine Wang <cawa@microsoft.com>
2022-10-04 10:22:50 -0700

    updating deployment script

--- quickstart_debugDemo.ps1
+++ quickstart_debugDemo.ps1
@@ -10,14 +10,15 @@ Param(
 )
 
 #Environment variables
-$SUBSCRIPTION_ID=""
-$DOCKERHUB_USERNAME=""
-$RESOURCE_GROUP=""
-$LOCATION=""
-$CONTAINERAPPS_ENVIRONMENT=""
-$DAPR_INSTRUMENTATION_KEY=""
-$LOGANALYTICS_WORKSPACE_ID=""
-$APPLICATIONINSIGHTS_CONNECTION_STRING=""
+$SUBSCRIPTION_ID="9de7ded6-0ad3-43e6-87b8-17d93e3ff695"
+$DOCKERHUB_USERNAME="cathyxwang"
+$RESOURCE_GROUP="PizzaOrderDemo3"
+$LOCATION="centralus"
+$CONTAINERAPPS_ENVIRONMENT="pizzaorderdemo3"
+$DAPR_INSTRUMENTATION_KEY="39f6f103-a3c2-4d38-a24e-3468da850aba"
+$LOGANALYTICS_WORKSPACE_ID="64d04329-08b5-4a54-b837-16ce633091ab"
+$LOGS_WORKSPACE_KEY="6U0XEefXN1DqGTMhOELM7BHG/4C6+cUC1LOByaq5Zg50ZgJBMECp/41zxs3PN3o183Gk26FnzfkntWRT8gmF0w=="
+$APPLICATIONINSIGHTS_CONNECTION_STRING="InstrumentationKey=39f6f103-a3c2-4d38-a24e-3468da850aba;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/"
 $ORDER_PROCESSOR_HTTP_URL=""
 
 #Connnect to an Azure subscription
@@ -34,14 +35,14 @@ az provider register --namespace Microsoft.OperationalInsights
 #Build docker images
 if(!$noDockerBuildWeb)
 {
-    docker build -t $DOCKERHUB_USERNAME/node-pizza-web-appinsights ./PizzaWeb/.
-    docker push $DOCKERHUB_USERNAME/node-pizza-web-appinsights
+    docker build -t $DOCKERHUB_USERNAME/node-pizza-web-appinsights-debug ./PizzaWeb/.
+    docker push $DOCKERHUB_USERNAME/node-pizza-web-appinsights-debug
 }
 
 if(!$noDockerBuildProcessor)
 {
-    docker build -t $DOCKERHUB_USERNAME/dotnet-pizza-backend-appinsights ./PizzaOrderProcessor/.
-    docker push $DOCKERHUB_USERNAME/dotnet-pizza-backend-appinsights
+    docker build -t $DOCKERHUB_USERNAME/dotnet-pizza-backend-appinsights-debug ./PizzaOrderProcessor/.
+    docker push $DOCKERHUB_USERNAME/dotnet-pizza-backend-appinsights-debug
 }
 
 
@@ -67,7 +68,7 @@ $envExists = $envCheck.Length -gt 0
 if (!$envExists)
 {
     Write-Output "Creating Container App Environment $CONTAINERAPPS_ENVIRONMENT ..."
-    az containerapp env create --name $CONTAINERAPPS_ENVIRONMENT --resource-group $RESOURCE_GROUP --location $LOCATION --dapr-instrumentation-key $DAPR_INSTRUMENTATION_KEY --logs-workspace-id $LOGANALYTICS_WORKSPACE_ID
+    az containerapp env create --name $CONTAINERAPPS_ENVIRONMENT --resource-group $RESOURCE_GROUP --location $LOCATION --dapr-instrumentation-key $DAPR_INSTRUMENTATION_KEY --logs-workspace-id $LOGANALYTICS_WORKSPACE_ID --logs-workspace-key $LOGS_WORKSPACE_KEY
 }
 else
 {
@@ -82,7 +83,7 @@ $pubsubExists = $pubsubCheck.Length -gt 0
 if (!$pubsubExists)
 {
     Write-Output "Creating Dapr component pubsub ..."
-    az containerapp env dapr-component set --name $CONTAINERAPPS_ENVIRONMENT --resource-group $RESOURCE_GROUP --dapr-component-name pubsub --yaml ./aca-dapr-components_local/pubsub.yaml
+    az containerapp env dapr-component set --name $CONTAINERAPPS_ENVIRONMENT --resource-group $RESOURCE_GROUP --dapr-component-name pubsub --yaml ./aca-dapr-components_PizzaOrderDemo3/pubsub.yaml
 }
 else
 {
@@ -96,7 +97,7 @@ $statestoreExists = $statestoreCheck.Length -gt 0
 if (!$statestoreExists)
 {
     Write-Output "Creating Dapr component statestore ..."
-    az containerapp env dapr-component set --name $CONTAINERAPPS_ENVIRONMENT --resource-group $RESOURCE_GROUP --dapr-component-name statestore --yaml ./aca-dapr-components_local/statestore.yaml
+    az containerapp env dapr-component set --name $CONTAINERAPPS_ENVIRONMENT --resource-group $RESOURCE_GROUP --dapr-component-name statestore --yaml ./aca-dapr-components_PizzaOrderDemo3/statestore.yaml
 }
 else
 {
@@ -108,16 +109,17 @@ $pizzaprocessingCheck = az containerapp list --environment $CONTAINERAPPS_ENVIRO
 $pizzaprocessingExists = $pizzaprocessingCheck.Length -gt 0
 if(!$pizzaprocessingExists)
 {
-    Write-Output "Creating containerapp order-web ..."
-    az containerapp create --name order-processor-http --resource-group $RESOURCE_GROUP --environment $CONTAINERAPPS_ENVIRONMENT --image $DOCKERHUB_USERNAME/dotnet-pizza-backend-appinsights:latest --target-port 80 --ingress external --min-replicas 1 --max-replicas 1 --enable-dapr --dapr-app-id order-processor-http --dapr-app-port 80 --env-vars `APPLICATIONINSIGHTS_CONNECTION_STRING=$APPLICATIONINSIGHTS_CONNECTION_STRING`
+    Write-Output "Creating containerapp order-processor-http ..."
+    az containerapp create --name order-processor-http --resource-group $RESOURCE_GROUP --yaml app.yaml
+
 }
 else
 {
     if(!$noDockerBuildProcessor)
     {
         $revisionSuffix = $((get-date).toString("yyyy-mm-dd-hhmmss"))
-        Write-output "Creating containerapp order-web revision with suffix $revisionSuffix"
-        az containerapp update --name order-processor-http --resource-group $RESOURCE_GROUP --image $DOCKERHUB_USERNAME/dotnet-pizza-backend-appinsights:latest --revision-suffix $revisionSuffix
+        Write-output "Creating containerapp order-processor-http revision with suffix $revisionSuffix"
+        az containerapp update --name order-processor-http --resource-group $RESOURCE_GROUP --image $DOCKERHUB_USERNAME/dotnet-pizza-backend-appinsights-debug:latest --revision-suffix $revisionSuffix
     }
     else
     {
@@ -129,12 +131,12 @@ else
 $pizzaprocessingCheck = az containerapp list --environment $CONTAINERAPPS_ENVIRONMENT --resource-group $RESOURCE_GROUP --query "[?name=='order-processor-http']" | ConvertFrom-Json
 $ORDER_PROCESSOR_HTTP_URL=$pizzaprocessingCheck.properties.Configuration.Ingress.fqdn
 #Deploy container app revisions
-$pizzawebCheck = az containerapp list --environment $CONTAINERAPPS_ENVIRONMENT --resource-group $RESOURCE_GROUP --query "[?name=='order-web']" | ConvertFrom-Json
+$pizzawebCheck = az containerapp list --environment $CONTAINERAPPS_ENVIRONMENT  --resource-group $RESOURCE_GROUP --query "[?name=='order-web']" | ConvertFrom-Json
 $pizzawebExists = $pizzawebCheck.Length -gt 0
 if(!$pizzawebExists)
 {
     Write-Output "Creating containerapp order-web ..."
-    az containerapp create --name order-web --resource-group $RESOURCE_GROUP --environment $CONTAINERAPPS_ENVIRONMENT  --image $DOCKERHUB_USERNAME/node-pizza-web-appinsights:latest --target-port 3000 --ingress external --min-replicas 1 --max-replicas 1 --enable-dapr --dapr-app-id order-web --dapr-app-port 3000 --env-vars `APPLICATIONINSIGHTS_CONNECTION_STRING=$APPLICATIONINSIGHTS_CONNECTION_STRING ORDER_PROCESSOR_HTTP_URL=$ORDER_PROCESSOR_HTTP_URL`
+    az containerapp create --name order-web --resource-group $RESOURCE_GROUP --environment $CONTAINERAPPS_ENVIRONMENT  --image $DOCKERHUB_USERNAME/node-pizza-web-appinsights-debug:latest --target-port 3000 --ingress external --min-replicas 1 --max-replicas 1 --enable-dapr --dapr-app-id order-web --dapr-app-port 3000 --env-vars `APPLICATIONINSIGHTS_CONNECTION_STRING=$APPLICATIONINSIGHTS_CONNECTION_STRING ORDER_PROCESSOR_HTTP_URL=$ORDER_PROCESSOR_HTTP_URL`
 }
 else
 {
@@ -142,7 +144,7 @@ else
     {
         $revisionSuffix = $((get-date).toString("yyyy-mm-dd-hhmmss"))
         Write-output "Creating containerapp order-web revision with suffix $revisionSuffix"
-        az containerapp update --name order-web --resource-group $RESOURCE_GROUP --image $DOCKERHUB_USERNAME/node-pizza-web-appinsights:latest --revision-suffix $revisionSuffix --set-env-vars `ORDER_PROCESSOR_HTTP_URL=$ORDER_PROCESSOR_HTTP_URL`
+        az containerapp update --name order-web --resource-group $RESOURCE_GROUP --image $DOCKERHUB_USERNAME/node-pizza-web-appinsights-debug:latest --revision-suffix $revisionSuffix --set-env-vars `ORDER_PROCESSOR_HTTP_URL=$ORDER_PROCESSOR_HTTP_URL`
     }
     else
     {
